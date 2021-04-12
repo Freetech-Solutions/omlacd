ff836649dfa7a83e47fd5aeb88ce75d3e0a71301
Andres Felipe Macias <felipe.macias@freetechsolutions.com.ar>
2021-04-12 17:03:47 +0000

    Oml 1729 dev creacion nuevo devenv

--- build/docker/acdbuilder/build_image.sh
+++ build/docker/acdbuilder/build_image.sh
@@ -6,6 +6,6 @@ if [ ! -f .asterisk_version ]; then
 fi
 ASTERISK_VERSION=$(cat .asterisk_version)
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-docker build -t freetechsolutions/omlapp-builder:$1 .
-docker push freetechsolutions/omlapp-builder:$1
+docker build -t freetechsolutions/omlacd-builder:$1 .
+docker push freetechsolutions/omlacd-builder:$1
 rm -rf .asterisk_version
