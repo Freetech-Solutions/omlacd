eb80182ab81596e57554df8e4924b4245a4a5f8a
fpignataro <>
2021-06-02 15:15:53 -0300

    quito audios de RPM, se descargan en after_install

--- build/rpm/build_rpm.sh
+++ build/rpm/build_rpm.sh
@@ -95,10 +95,9 @@ echo "Installing the requirements packages"
 pip3 install wheel
 pip3 install -r build/docker/acdbuilder/requirements.txt --exists-action 'w'
 
-echo "Adding conf, agis, logrotate and sounds omnileads"
+echo "Adding conf, agis, logrotate and legacy scripts omnileads"
 cp -a source/astconf/* ${ASTERISK_LOCATION}/etc/asterisk/
 cp -a source/agis/* ${ASTERISK_LOCATION}/var/lib/asterisk/agi-bin/
-cp -a source/sounds/* ${ASTERISK_LOCATION}/var/lib/asterisk/sounds/
 cp -a source/scripts/* ${VIRTUALENV_LOCATION}
 
 echo "Packing the rpm"
