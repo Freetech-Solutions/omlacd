c7d77868039a7251b24cd951d22f1ed18e601761
fpignataro <>
2021-06-28 14:49:31 -0300

    fix entrypoint touch custom and override files

--- build/docker/docker-entrypoint.sh
+++ build/docker/docker-entrypoint.sh
@@ -44,52 +44,145 @@ if [ "$1" == "" ]; then
     /usr/sbin/adduser --gecos "" --no-create-home --uid 1000 --disabled-password ${ASTERISK_USER} || exit
   fi
 
-  touch oml_amd_custom.conf
-  touch oml_dahdi_custom.conf
-  touch oml_extensions_bridgecall_custom.conf
-  touch oml_extensions_commonsub_custom.conf
-  touch oml_extensions_custom.conf
-  touch oml_extensions_globals_custom.conf
-  touch oml_extensions_inr_custom.conf
-  touch oml_extensions_ivr_custom.conf
-  touch oml_extensions_modules_custom.conf
-  touch oml_extensions_outr_custom.conf
-  touch oml_extensions_postcall_custom.conf
-  touch oml_extensions_precall_custom.conf
-  touch oml_extensions_tc_custom.conf
-  touch oml_func_odbc_custom.conf
-  touch oml_http_custom.conf
-  touch oml_manager_custom.conf
-  touch oml_pjsip_custom.conf
-  touch oml_pjsip_wizard_custom.conf
-  touch oml_queues_custom.conf
-  touch oml_res_odbc_custom.conf
-  touch oml_sip_general_custom.conf
-  touch oml_sip_registrations_custom.conf
-  touch oml_sip_trunks_custom.conf
-  touch oml_amd_override.conf
-  touch oml_dahdi_override.conf
-  touch oml_extensions_override.conf
-  touch oml_extensions_bridgecall_override.conf
-  touch oml_extensions_commonsub_override.conf
-  touch oml_extensions_globals_override.conf
-  touch oml_extensions_modules_override.conf
-  touch oml_extensions_outr_override.conf
-  touch oml_extensions_override.conf
-  touch oml_extensions_postcall_override.conf
-  touch oml_extensions_precall_override.conf
-  touch oml_func_odbc_override.conf
-  touch oml_http_override.conf
-  touch oml_manager_override.conf
-  touch oml_pjsip_override.conf
-  touch oml_pjsip_wizard_override.conf
-  touch oml_queues_override.conf
-  touch oml_res_odbc_override.conf
-  touch oml_sip_general_override.conf
-  touch oml_sip_registrations_override.conf
-  touch oml_sip_trunks_override.conf
-  touch oml_voicemail_custom.conf
-  touch oml_voicemail_override.conf
+  cd /etc/asterisk
+  if [ ! -e  "oml_amd_custom.conf" ]; then 
+    touch oml_amd_custom.conf 
+  fi
+  if [ ! -e  "oml_dahdi_custom.conf" ]; then 
+    touch oml_dahdi_custom.conf 
+  fi
+  if [ ! -e  "oml_extensions_bridgecall_custom.conf" ]; then 
+    touch oml_extensions_bridgecall_custom.conf 
+  fi
+  if [ ! -e  "oml_extensions_commonsub_custom.conf" ]; then 
+    touch oml_extensions_commonsub_custom.conf 
+  fi
+  if [ ! -e  "oml_extensions_custom.conf" ]; then 
+    touch oml_extensions_custom.conf 
+  fi
+  if [ ! -e  "oml_extensions_globals_custom.conf" ]; then 
+    touch oml_extensions_globals_custom.conf 
+  fi
+  if [ ! -e  "oml_extensions_inr_custom.conf" ]; then
+    touch oml_extensions_inr_custom.conf 
+  fi
+  if [ ! -e  "oml_extensions_ivr_custom.conf" ]; then 
+    touch oml_extensions_ivr_custom.conf 
+  fi
+  if [ ! -e  "oml_extensions_modules_custom.conf" ]; then 
+    touch oml_extensions_modules_custom.conf 
+  fi
+  if [ ! -e  "oml_extensions_outr_custom.conf" ]; then 
+    touch oml_extensions_outr_custom.conf 
+  fi
+  if [ ! -e  "oml_extensions_postcall_custom.conf" ]; then 
+    touch oml_extensions_postcall_custom.conf 
+  fi
+  if [ ! -e  "oml_extensions_precall_custom.conf" ]; then 
+    touch oml_extensions_precall_custom.conf 
+  fi
+  if [ ! -e  "oml_extensions_tc_custom.conf" ]; then 
+    touch oml_extensions_tc_custom.conf 
+  fi
+  if [ ! -e  "oml_func_odbc_custom.conf" ]; then 
+    touch oml_func_odbc_custom.conf 
+  fi
+  if [ ! -e  "oml_http_custom.conf" ]; then 
+    touch oml_http_custom.conf 
+  fi
+  if [ ! -e  "oml_manager_custom.conf" ]; then 
+    touch oml_manager_custom.conf 
+  fi
+  if [ ! -e  "oml_pjsip_custom.conf" ]; then 
+    touch oml_pjsip_custom.conf 
+  fi
+  if [ ! -e  "oml_pjsip_wizard_custom.conf" ]; then 
+    touch oml_pjsip_wizard_custom.conf 
+  fi
+  if [ ! -e  "oml_queues_custom.conf" ]; then 
+    touch oml_queues_custom.conf 
+  fi
+  if [ ! -e  "oml_res_odbc_custom.conf" ]; then 
+    touch oml_res_odbc_custom.conf 
+  fi
+  if [ ! -e  "oml_sip_general_custom.conf" ]; then 
+    touch oml_sip_general_custom.conf 
+  fi
+  if [ ! -e  "oml_sip_registrations_custom.conf" ]; then 
+    touch oml_sip_registrations_custom.conf 
+  fi
+  if [ ! -e  "oml_sip_trunks_custom.conf" ]; then 
+    touch oml_sip_trunks_custom.conf 
+  fi
+  if [ ! -e  "oml_amd_override.conf" ]; then 
+    touch oml_amd_override.conf 
+  fi
+  if [ ! -e  "oml_dahdi_override.conf" ]; then 
+    touch oml_dahdi_override.conf 
+  fi
+  if [ ! -e  "oml_extensions_override.conf" ]; then 
+    touch oml_extensions_override.conf 
+  fi
+  if [ ! -e  "oml_extensions_bridgecall_override.conf" ]; then 
+    touch oml_extensions_bridgecall_override.conf 
+  fi
+  if [ ! -e  "oml_extensions_commonsub_override.conf" ]; then 
+    touch oml_extensions_commonsub_override.conf 
+  fi
+  if [ ! -e  "oml_extensions_globals_override.conf" ]; then 
+    touch oml_extensions_globals_override.conf 
+  fi
+  if [ ! -e  "oml_extensions_modules_override.conf" ]; then 
+    touch oml_extensions_modules_override.conf 
+  fi
+  if [ ! -e  "oml_extensions_outr_override.conf" ]; then 
+    touch oml_extensions_outr_override.conf 
+  fi
+  if [ ! -e  "oml_extensions_override.conf" ]; then 
+    touch oml_extensions_override.conf 
+  fi
+  if [ ! -e  "oml_extensions_postcall_override.conf" ]; then 
+    touch oml_extensions_postcall_override.conf 
+  fi
+  if [ ! -e  "oml_extensions_precall_override.conf" ]; then 
+    touch oml_extensions_precall_override.conf 
+  fi
+  if [ ! -e  "oml_func_odbc_override.conf" ]; then 
+    touch oml_func_odbc_override.conf 
+  fi
+  if [ ! -e  "oml_http_override.conf" ]; then 
+    touch oml_http_override.conf 
+  fi
+  if [ ! -e  "oml_manager_override.conf" ]; then 
+    touch oml_manager_override.conf 
+  fi
+  if [ ! -e  "oml_pjsip_override.conf" ]; then 
+    touch oml_pjsip_override.conf 
+  fi
+  if [ ! -e  "oml_pjsip_wizard_override.conf" ]; then 
+    touch oml_pjsip_wizard_override.conf 
+  fi
+  if [ ! -e  "oml_queues_override.conf" ]; then 
+    touch oml_queues_override.conf 
+  fi
+  if [ ! -e  "oml_res_odbc_override.conf" ]; then 
+    touch oml_res_odbc_override.conf 
+  fi
+  if [ ! -e  "oml_sip_general_override.conf" ]; then 
+    touch oml_sip_general_override.conf 
+  fi
+  if [ ! -e  "oml_sip_registrations_override.conf" ]; then 
+    touch oml_sip_registrations_override.conf 
+  fi
+  if [ ! -e  "oml_sip_trunks_override.conf" ]; then 
+    touch oml_sip_trunks_override.conf 
+  fi
+  if [ ! -e  "oml_voicemail_custom.conf" ]; then 
+    touch oml_voicemail_custom.conf 
+  fi
+  if [ ! -e  "oml_voicemail_override.conf" ]; then 
+    touch oml_voicemail_override.conf 
+  fi
 
   cd /usr/src
   echo "Download en Asterisk sounds"
