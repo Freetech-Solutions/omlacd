f57c026dd5bf9fdd2a2a91afeb0b72a31cee7ce2
fpignataro <2620878-psychodoom@users.noreply.gitlab.com>
2022-02-14 11:11:28 +0000

    Develop

--- build/docker/docker-entrypoint.sh
+++ build/docker/docker-entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # run as user asterisk by default
-ASTERISK_USER=${ASTERISK_USER:-asterisk}
 ASTERISK_AUDIO_PROMPTS=https://downloads.asterisk.org/pub/telephony/sounds/asterisk-core-sounds-en-alaw-current.tar.gz
 OMNILEADS_AUDIO_PROMPTS=https://fts-public-packages.s3-sa-east-1.amazonaws.com/asterisk/asterisk-oml-sounds-current.tar.gz
 PUBLIC_IP=$(curl http://ipinfo.io/ip)
 
 set -ex
-COMMAND="/usr/sbin/asterisk -T -U ${ASTERISK_USER} -p -vvvvvvvf"
+COMMAND="/usr/sbin/asterisk -T -U asterisk -p -vvvvvvvf"
 
 if [ "$1" == "" ]; then
   echo "**[omlacd] Setting localtime"
@@ -17,32 +16,14 @@ if [ "$1" == "" ]; then
   sed -i "s/amiuser/$AMI_USER/g" /etc/asterisk/oml_manager.conf
   sed -i "s/amipassword/$AMI_PASSWORD/g" /etc/asterisk/oml_manager.conf
 
-  echo "**[omlacd] Writting the IP in pjsip files"
-  sed -i "0,/external_media_address=.*/s//external_media_address=${PUBLIC_IP}/g" /etc/asterisk/oml_pjsip_transports.conf
-  sed -i "0,/external_signaling_address=.*/s//external_signaling_address=${PUBLIC_IP}/g" /etc/asterisk/oml_pjsip_transports.conf
-  sed -i "0,/external_media_address=.*/! s/external_media_address=.*/external_media_address=${DOCKER_IP}/" /etc/asterisk/oml_pjsip_transports.conf
-  sed -i "0,/external_signaling_address=.*/! s/external_signaling_address=.*/external_signaling_address=${DOCKER_IP}/" /etc/asterisk/oml_pjsip_transports.conf
-
   sed -i "s/^;queue_log_realtime_use_gmt=yes/queue_log_realtime_use_gmt=yes/g" /etc/asterisk/logger.conf
-
 
   echo "**[omlacd] Writing the odbc.ini file with database variables"
   sed -i "s/Servername.*/Servername         = ${PGHOST}/g" /etc/odbc.ini
   sed -i "s/^Database.*/Database            = ${PGDATABASE}/g" /etc/odbc.ini
   sed -i "s/^UserName.*/UserName            = ${PGUSER}/g" /etc/odbc.ini
   sed -i "s/^Password.*/Password            = ${PGPASSWORD}/g" /etc/odbc.ini
-  sed -i "s/^Port.*/Port            = ${PGPORT}/g" /etc/odbc.ini
-
-  echo "**[omlacd] Writing oml_res_odbc.conf file"
-  sed -i "s/^username.*/username => ${PGUSER}/g" /etc/asterisk/oml_res_odbc.conf
-  # recreate user and group for asterisk
-  # if they've sent as env variables (i.e. to macth with host user to fix permissions for mounted folders
-  #deluser asterisk
-  if id -u $ASTERISK_USER; then
-    echo "**[omlacd] Asterisk user already created"
-  else
-    /usr/sbin/adduser --gecos "" --no-create-home --uid 1000 --disabled-password ${ASTERISK_USER} || exit
-  fi
+  sed -i "s/^Port.*/Port                    = ${PGPORT}/g" /etc/odbc.ini
 
   cd /etc/asterisk
   if [ ! -e  "oml_amd_custom.conf" ]; then 
