c03d9c406983fdb0e63ec018165371efedb6c9ac
fpignataro <fabian.pignataro84@gmail.com>
2022-07-28 11:50:21 -0300

    sounds afuera y build cicd

--- build/docker/build_image.sh
+++ build/docker/build_image.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 set -e
 
-ASTERISK_VERSION=$1
+ASTERISK_VERSION=$(cat ../../.package_version)
 IMG_TAG=$2
 
-echo "$ASTERISK_VERSION" > .asterisk_version
+if [ "$2"  ]; then
+  echo "$ASTERISK_VERSION" > .asterisk_version
+  docker build -f Dockerfile -t freetechsolutions/omlacd:$IMG_TAG ../..
+  docker push freetechsolutions/omlacd:$IMG_TAG
+  rm -rf .asterisk_version
+else
+  docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
+  echo "$ASTERISK_VERSION" > .asterisk_version
+  docker build -f Dockerfile -t freetechsolutions/omlacd:$IMG_TAG ../..
+  docker push freetechsolutions/omlacd:$IMG_TAG
+  rm -rf .asterisk_version
+fi
 
-docker build -f Dockerfile -t freetechsolutions/omlacd:$IMG_TAG ../..
-docker push freetechsolutions/omlacd:$IMG_TAG
-
-rm -rf .asterisk_version
+if [ $CI_COMMIT_REF_NAME == "master" ]; then
+  docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
+  echo "$ASTERISK_VERSION" > .asterisk_version
+  docker build -f Dockerfile -t freetechsolutions/omlacd:$IMG_TAG ../..
+  docker push freetechsolutions/omlacd:$IMG_TAG
+  rm -rf .asterisk_version
+elif [ $CI_COMMIT_REF_NAME == "develop" ]; then
+  docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
+  echo "$ASTERISK_VERSION" > .asterisk_version
+  docker build -f Dockerfile -t freetechsolutions/omlacd:$IMG_TAG ../..
+  docker push freetechsolutions/omlacd:$IMG_TAG
+  rm -rf .asterisk_version
+fi
