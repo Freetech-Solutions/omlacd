d10d39ae727c5cc905ce38d3a84f50366f64e1f4
fpignataro <fabian.pignataro84@gmail.com>
2022-09-06 00:21:15 -0300

    envvars exclusivas

--- deploy/templates/asterisk_transition.sh
+++ deploy/templates/asterisk_transition.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "$(date): transition to master" >> /var/log/keepalive_transition.log
-source /etc/profile.d/omnileads_envars.sh && source /opt/omnileads/asterisk/virtualenv/bin/activate && /opt/omnileads/asterisk/virtualenv/bin/python3 /opt/omnileads/asterisk/virtualenv/asterisk_transition.py >> /var/log/keepalive_transition.log 2>&1
+source /etc/omnileads-asterisk.env && source /opt/omnileads/asterisk/virtualenv/bin/activate && /opt/omnileads/asterisk/virtualenv/bin/python3 /opt/omnileads/asterisk/virtualenv/asterisk_transition.py >> /var/log/keepalive_transition.log 2>&1
 
 exit 0
