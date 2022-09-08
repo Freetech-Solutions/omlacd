ef9134d34439470cc425ab631e9d6bc650faee97
fpignataro <fabian.pignataro84@gmail.com>
2022-09-07 21:23:18 -0300

    debian 11

--- build/rpm/builder_container.sh
+++ build/rpm/builder_container.sh
@@ -17,4 +17,4 @@ docker run -it --rm --name asterisk-fpm \
   --mount type=bind,source="$(pwd)"/../..,target=/builds/omnileads/omlacd \
   --env-file .env \
   --network=host --workdir=/builds/omnileads/omlacd/ \
-  freetechsolutions/fpm-asterisk:dev bash
+  freetechsolutions/fpm-asterisk:220907.01 bash
