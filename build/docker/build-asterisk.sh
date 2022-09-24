62f9b161217b3e3f1044731966c608404a1198f4
fpignataro <fabian.pignataro84@gmail.com>
2022-09-24 18:08:30 -0300

    build img

--- build/docker/build-asterisk.sh
+++ build/docker/build-asterisk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PROGNAME=$(basename $0)
 
-ASTERISK_VERSION=$(cat ../../.asterisk_version)
+ASTERISK_VERSION=$(cat /opt/.asterisk_version)
 
 if test -z ${ASTERISK_VERSION}; then
   echo "${PROGNAME}: ASTERISK_VERSION required" >&2
