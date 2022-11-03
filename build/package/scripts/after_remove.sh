83f617f41e55f8a87324c537bfc78912cd1e5245
fpignataro <fabian.pignataro84@gmail.com>
2022-11-03 16:48:31 -0300

    build solo img, sounds into img

--- build/package/scripts/after_remove.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script that runs after asterisk remove
-# echo "Removing asterisk symbolic links and folders"
-# rm -rf /usr/sbin/asterisk
-# rm -rf /opt/omnileads/asterisk
-# rm -rf /etc/odbc.ini
-# rm -rf /usr/lib64/psqlodbcw.so
