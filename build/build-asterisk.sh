cc9ff617e15881fb5a889baf10a58c45f9955efe
fpignataro <fabian.pignataro84@gmail.com>
2022-11-04 06:54:35 -0300

    untar audios

--- build/build-asterisk.sh
+++ build/build-asterisk.sh
@@ -131,14 +131,14 @@ cd /
 
 echo "Download en Asterisk sounds"
 mkdir -p /var/lib/asterisk/sounds/en
-curl -s $ASTERISK_AUDIO_PROMPTS
-tar xvz asterisk-core-sounds-en-alaw-current.tar.gz -C /var/lib/asterisk/sounds/en
+wget $ASTERISK_AUDIO_PROMPTS
+tar xvfz asterisk-core-sounds-en-alaw-current.tar.gz -C /var/lib/asterisk/sounds/en
 rm -f asterisk-core-sounds-en-alaw-current.tar.gz
 
 echo "Download OMniLeads sounds"
 mkdir -p /var/lib/asterisk/sounds/oml
-curl -s $OMNILEADS_AUDIO_PROMPTS
-tar xvz asterisk-oml-sounds-current.tar.gz -C /var/lib/asterisk/sounds/oml
+wget $OMNILEADS_AUDIO_PROMPTS
+tar xvfz asterisk-oml-sounds-current.tar.gz -C /var/lib/asterisk/sounds/oml
 rm -f asterisk-oml-sounds-current.tar.gz
 
 chmod -R 750 /var/lib/asterisk/sounds
