ef123f07a24213c32599603c13c81280278d8bf6
fpignataro <fabian.pignataro84@gmail.com>
2023-01-25 11:37:33 -0300

    callrec_move flags no-check-cert

--- source/agis/omni-callrec-move.sh
+++ source/agis/omni-callrec-move.sh
@@ -7,22 +7,13 @@ Lsof="/sbin/lsof"
 DirectorioFinal=/opt/callrec/$Ano-$Mes-$Dia
 
 case ${CALLREC_DEVICE} in
-    nfs)
-	    if [ ! -d $DirectorioFinal ];then
-          mkdir $DirectorioFinal
-        fi
-        mv ${ASTERISK_LOCATION}/var/spool/asterisk/monitor/$Ano-$Mes-$Dia/$1 $DirectorioFinal
-        ;;
     s3-aws)
-        aws s3 mv ${ASTERISK_LOCATION}/var/spool/asterisk/monitor/$Ano-$Mes-$Dia/$1 s3://${S3_BUCKET_NAME}/$Ano-$Mes-$Dia/
+        aws s3 mv /var/spool/asterisk/monitor/$Ano-$Mes-$Dia/$1 s3://${S3_BUCKET_NAME}/$Ano-$Mes-$Dia/
         ;;
-    s3-minio)
-    	aws --endpoint-url ${S3_ENDPOINT} --no-verify-ssl s3 mv ${ASTERISK_LOCATION}/var/spool/asterisk/monitor/$Ano-$Mes-$Dia/$1 s3://${S3_BUCKET_NAME}/$Ano-$Mes-$Dia/
-        ;;       
-    s3-devenv)
-    	aws --endpoint-url ${S3_ENDPOINT} s3 mv /var/spool/asterisk/monitor/$Ano-$Mes-$Dia/$1 s3://${S3_BUCKET_NAME}/$Ano-$Mes-$Dia/
-        ;;           
+    s3-no-check-cert)
+    	aws --endpoint-url ${S3_ENDPOINT} --no-verify-ssl s3 mv /var/spool/asterisk/monitor/$Ano-$Mes-$Dia/$1 s3://${S3_BUCKET_NAME}/$Ano-$Mes-$Dia/
+        ;;
     *)
-    	aws --endpoint-url ${S3_ENDPOINT} s3 mv ${ASTERISK_LOCATION}/var/spool/asterisk/monitor/$Ano-$Mes-$Dia/$1 s3://${S3_BUCKET_NAME}/$Ano-$Mes-$Dia/
-        ;;               
+    	aws --endpoint-url ${S3_ENDPOINT} s3 mv /var/spool/asterisk/monitor/$Ano-$Mes-$Dia/$1 s3://${S3_BUCKET_NAME}/$Ano-$Mes-$Dia/
+        ;;
 esac
