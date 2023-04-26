8e4e3034047049c6832a084a458f2b43844bef61
fpignataro <fabian.pignataro84@gmail.com>
2023-04-26 20:10:49 -0300

    fix entrypoint syntax

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -22,10 +22,10 @@ if [ "$1" == "" ]; then
   # Set AMI listen IPADDR 
   # Set SIP-Agent(5160) listen IPADDR & SIP-PSTN(5060) listen IPADDR
   case ${ENV} in
-  docker-compose-cloud))
+  docker-compose-devenv)
     echo "devenv docker-compose"
     sed -i "s/50000/40099/g" /etc/asterisk/rtp.conf
-    sed -i "s/bindaddr=127.0.0.1/bindaddr=0.0.0.0/g" /etc/asterisk/oml_manager.conf    
+    sed -i "s/bindaddr=127.0.0.1/bindaddr=0.0.0.0/g" /etc/asterisk/oml_manager.conf
     ;;
   docker-compose-cloud)
     echo "cloud docker-compose"
