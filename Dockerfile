9b3b8617003db093d549d3fb5d4236feca611283
fpignataro <2620878-fpignataro@users.noreply.gitlab.com>
2025-03-14 12:47:51 +0000

    Release 2.4.0

--- Dockerfile
+++ Dockerfile
@@ -1,61 +1,15 @@
-FROM python:3.12-slim-bullseye AS dev
+FROM omnileads/asterisk_base_img:250222.01 as run
 
 ENV LANG en_US.utf8
 ENV NOTVISIBLE "in users profile"
 
-RUN pip install --upgrade pip
+COPY build/requirements.txt /
 
-COPY build/build-asterisk.sh .asterisk_version build/requirements.txt /
-RUN ASTERISK_VERSION=$(cat .asterisk_version) /build-asterisk.sh
 RUN echo "deb http://ftp.de.debian.org/debian bullseye main non-free" >> /etc/apt/sources.list
-RUN apt update \
-    && apt install -y libgsm1 git curl gnupg libttspico-utils sox \
-    && pip install -r /requirements.txt \
-    && apt-get update -y \
-    && apt install sngrep -y \
-    && apt-get remove --purge git -y \
-    && apt autoremove -y \
-    && mkdir /root/bin/  \
-    && cp /usr/sbin/ast* /usr/bin/curl /usr/bin/sngrep /root/bin \    
-    && cp -a /lib/x86_64-linux-gnu/libkeyutils.so.1* /usr/lib/x86_64-linux-gnu/ \
-    && rm -rf /usr/src/asterisk \
-    && rm -rf  /usr/sbin/ast* \
-    && rm -rf /usr/include/asterisk \
-    && rm -rf /lib/x86_64-linux-gnu/libkeyutils.so.1*
-
-FROM python:3.12-slim-bullseye AS run
-
-RUN apt update -qq \
-    && apt install -y libbinutils libedit2 libncursesw5 wget awscli \
-    && apt autoremove -y \
-    && apt clean -y \
-    && apt purge -y \
-    && rm -rf /var/lib/apt/lists/*
-
-# Copia de los componentes de Asterisk y picoTTS desde la etapa de desarrollo
-COPY --from=dev /usr/local /usr/local
-# Asterisk
-COPY --from=dev /usr/lib/libasterisk* /usr/lib/
-COPY --from=dev /etc/asterisk /etc/asterisk/
-COPY --from=dev /var/lib/asterisk /var/lib/asterisk
-COPY --from=dev /var/log/asterisk /var/log/asterisk
-COPY --from=dev /var/spool/asterisk /var/spool/asterisk
-COPY --from=dev /root/bin/* /usr/sbin/
-COPY --from=dev /usr/lib/asterisk /usr/lib/asterisk/
-COPY --from=dev /var/run/asterisk/ /var/run/asterisk/
-# picoTTS
-COPY --from=dev /usr/share/pico/lang/ /usr/share/pico/lang/
-COPY --from=dev /usr/bin/pico2wave /usr/bin/pico2wave
-# Sox
-COPY --from=dev /usr/bin/sox /usr/bin/sox 
-COPY --from=dev /usr/lib/mime/packages/sox /usr/lib/mime/packages/sox
-COPY --from=dev /usr/bin/play /usr/bin/play
-COPY --from=dev /usr/bin/rec /usr/bin/rec
-COPY --from=dev /usr/bin/soxi /usr/bin/soxi
-
-# Librerias de python, curl otras librerias necesarias
-COPY --from=dev /usr/local/lib/python3.12/ /usr/local/lib/python3.12/
-COPY --from=dev /usr/lib/x86_64-linux-gnu/ /usr/lib/x86_64-linux-gnu/
+RUN apt update && apt install -y --no-install-recommends \
+    sngrep curl \
+    && pip install --no-cache-dir --upgrade pip && pip install --no-cache-dir -r /requirements.txt \
+    && apt autoremove -y && apt clean -y && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 COPY source/astconf/* /etc/asterisk/
 COPY source/astconf/retrieve_conf/ /etc/asterisk/retrieve_conf/
@@ -63,9 +17,9 @@ COPY source/scripts/* /opt/asterisk/scripts/
 COPY source/agi-bin/* /var/lib/asterisk/agi-bin/
 COPY build/docker-entrypoint.sh /docker-entrypoint.sh
 
-RUN chmod 750 -R /var/spool/asterisk
-RUN useradd -M omnileads
-RUN chown -R omnileads.omnileads /var/lib/asterisk /etc/asterisk /opt/asterisk /usr/lib/asterisk /docker-entrypoint.sh /var/spool/asterisk /var/log/asterisk
+RUN chmod 750 -R /var/spool/asterisk && \
+    useradd -M omnileads && \
+    chown -R omnileads:omnileads /var/lib/asterisk /etc/asterisk /opt/asterisk /usr/lib/asterisk /docker-entrypoint.sh /var/spool/asterisk /var/log/asterisk
 
 EXPOSE 5060/udp
 
--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -23,7 +23,7 @@ if [ "$1" == "" ]; then
   echo "**[omlacd] Writting the AMI config"
   sed -i "s/amiuser/$AMI_USER/g" /etc/asterisk/oml_manager.conf
   sed -i "s/amipassword/$AMI_PASSWORD/g" /etc/asterisk/oml_manager.conf
-  
+
   # Set ARI user & password
   echo "**[omlacd] Writting the ARI config"
   sed -i "s/ariuser/$AMI_USER/g" /etc/asterisk/oml_ari.conf
@@ -33,7 +33,6 @@ if [ "$1" == "" ]; then
   case ${ENV} in
     devenv)
       echo "devenv docker-compose"
-      sed -i "s/50000/40099/g" /etc/asterisk/rtp.conf
       sed -i "s/bindaddr=127.0.0.1/bindaddr=0.0.0.0/g" /etc/asterisk/oml_manager.conf
       sed -i "s/bindaddr=127.0.0.1/bindaddr=0.0.0.0/g" /etc/asterisk/oml_http.conf
       ;;
@@ -60,6 +59,8 @@ if [ "$1" == "" ]; then
       if [[ "${ARQ}" == "cluster" ]]; then
         sed -i "s/bindaddr=127.0.0.1/bindaddr=$ASTERISK_HOSTNAME/g" /etc/asterisk/oml_http.conf
       fi
+      sed -i "s/bind=0.0.0.0:5060/bind=$PUBLIC_IP:5060/g" /etc/asterisk/oml_pjsip_transports.conf
+      sed -i "s/10.22.22.199:5060/$PUBLIC_IP:5060/g" /etc/asterisk/oml_pjsip_wizard.conf
       ;;
     cloud_external_dialer)
       echo "******* cloud scenary with external dialer *******"
@@ -69,6 +70,7 @@ if [ "$1" == "" ]; then
       if [[ "${ARQ}" == "cluster" ]]; then
         sed -i "s/bindaddr=127.0.0.1/bindaddr=$ASTERISK_HOSTNAME/g" /etc/asterisk/oml_http.conf
       fi
+      sed -i "s/10.22.22.199:5060/$DIALER_ACD_HOSTNAME:5060/g" /etc/asterisk/oml_pjsip_transports.conf
       ;;  
     lan)
       echo "******** lan scenary *******"
@@ -78,6 +80,7 @@ if [ "$1" == "" ]; then
       if [[ "${ARQ}" == "cluster" ]]; then
         sed -i "s/bindaddr=127.0.0.1/bindaddr=$ASTERISK_HOSTNAME/g" /etc/asterisk/oml_http.conf
       fi
+      sed -i "s/10.22.22.199:5060/$DIALER_ACD_HOSTNAME:5060/g" /etc/asterisk/oml_pjsip_wizard.conf
       ;;    
     nat)
       echo "********* nat scenary *******"
@@ -89,6 +92,7 @@ if [ "$1" == "" ]; then
       if [[ "${ARQ}" == "cluster" ]]; then
         sed -i "s/bindaddr=127.0.0.1/bindaddr=$ASTERISK_HOSTNAME/g" /etc/asterisk/oml_http.conf
       fi
+      sed -i "s/10.22.22.199:5060/$DIALER_ACD_HOSTNAME:5060/g" /etc/asterisk/oml_pjsip_transports.conf
       ;;
     hybrid)
       echo "********* hybrid scenary *******"
@@ -98,6 +102,7 @@ if [ "$1" == "" ]; then
       if [[ "${ARQ}" == "cluster" ]]; then
         sed -i "s/bindaddr=127.0.0.1/bindaddr=$ASTERISK_HOSTNAME/g" /etc/asterisk/oml_http.conf
       fi
+      sed -i "s/10.22.22.199:5060/$DIALER_ACD_HOSTNAME:5060/g" /etc/asterisk/oml_pjsip_transports.conf
       ;;
     all)
       echo "******* open 0.0.0.0 scenary *******"
@@ -105,6 +110,7 @@ if [ "$1" == "" ]; then
       sed -i "s/bind=0.0.0.0:5160/bind=0.0.0.0:5160/g" /etc/asterisk/oml_pjsip_transports.conf
       sed -i "s/bind=0.0.0.0:5060/bind=0.0.0.0:5060/g" /etc/asterisk/oml_pjsip_transports.conf
       sed -i "s/bindaddr=127.0.0.1/bindaddr=0.0.0.0/g" /etc/asterisk/oml_http.conf  
+      sed -i "s/10.22.22.199:5060/$DIALER_ACD_HOSTNAME:5060/g" /etc/asterisk/oml_pjsip_transports.conf
       ;;
     ha)
       echo "******** HA scenary *******"
@@ -112,6 +118,7 @@ if [ "$1" == "" ]; then
       sed -i "s/bind=0.0.0.0:5160/bind=$ASTERISK_HOSTNAME:5160/g" /etc/asterisk/oml_pjsip_transports.conf
       sed -i "s/bind=0.0.0.0:5060/bind=$ASTERISK_VIP:5060/g" /etc/asterisk/oml_pjsip_transports.conf
       sed -i "s/bindaddr=127.0.0.1/bindaddr=$ASTERISK_VIP/g" /etc/asterisk/oml_http.conf
+      sed -i "s/10.22.22.199:5060/$DIALER_ACD_HOSTNAME:5060/g" /etc/asterisk/oml_pjsip_transports.conf
       ;;      
     *)
       echo "You must to pass ENV var: devenv, cloud, lan or nat"
@@ -123,7 +130,7 @@ if [ "$1" == "" ]; then
   # Set HOMER heplify 
   if [[ "${HOMER_ENABLE}" == "True" ]]; then
     sed -i "s/no/yes/g" /etc/asterisk/hep.conf
-    sed -i "s/homer_host:homer_port/$HOMERHOST:$HOMERPORT/g" /etc/asterisk/hep.conf
+    sed -i "s/homer_host:homer_port/$HOMER_HOST:$HOMER_PORT/g" /etc/asterisk/hep.conf
     sed -i "s/tenant/$TENANT_ID/g" /etc/asterisk/hep.conf
   fi
 
@@ -136,6 +143,11 @@ if [ "$1" == "" ]; then
     sed -i "s/threadpool_idle_timeout=60/threadpool_idle_timeout=${THREADPOOL_IDLE_TIMEOUT}/g" /etc/asterisk/oml_pjsip.conf
     sed -i "s/threadpool_max_size=50/threadpool_max_size=${THREADPOOL_MAX_SIZE}/g" /etc/asterisk/oml_pjsip.conf
   fi
+
+# Replace docker dialer_acd:5060 host, if DIALER_HOSTNAME is set
+if [[ -n "${DIALER_HOST}" ]]; then
+    sed -i "s/dialer-asterisk/${DIALER_HOST}/g" /etc/asterisk/oml_pjsip_wizard.conf
+fi
 
 else
   echo "**[omlacd] Initializing regenerar_asterisk script"
